9c0f45b0ea19cf5e0d9f0580e37a2247ce7d210d
Mochichi2003 <44772513+Mochichi2003@users.noreply.github.com>
2020-09-25 22:48:32 +0900

    ライブのカウントダウンページを作り始めた

--- components/header.tsx
+++ components/header.tsx
@@ -13,8 +13,8 @@ export default function header() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/live">LivePage</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import { AppProps } from "next/app";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../style/global.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   );
 }
