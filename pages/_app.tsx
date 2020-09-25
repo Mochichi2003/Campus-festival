15afd629012d38ea8a858a2df0929d605332cb10
Mochichi2003 <moyurusuto@gmail.com>
2020-09-25 15:05:13 +0900

    headerを作ってるとこ

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import { AppProps } from "next/app";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />;
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
+
 import React from "react";
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>HEY 👋</h1>
+
     <p>
       <Link href="/about">
         <a>About</a>
