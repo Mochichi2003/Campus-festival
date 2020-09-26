6ba1fce512619411be7d54801ec019e4ce4ce1d0
Mochichi2003 <44772513+Mochichi2003@users.noreply.github.com>
2020-09-26 23:45:28 +0900

    わからぬ

--- components/Countdowntimer/CountdownTimer.tsx
+++ components/Countdowntimer/CountdownTimer.tsx
@@ -1,21 +1,40 @@
 import React from "react";
 
 import styles from "./CountdownTimer.module.sass";
-import moment from "moment";
-moment().locale("ja");
+// import moment from "moment";
+import dayjs from "dayjs";
+
+// moment().locale("ja");
 class RemainingTime extends React.Component {
+  // TODO  カウントダウンを表示するようにする
+  nowtime(): string {
+    return dayjs().format("残り DD日 hh時間mm分ss秒");
+  }
+
   constructor(props: any) {
     super(props);
-    this.state = {
-      timer: moment().format("YYYY-MM-DD HH:mm:ss"),
-    };
+    this.state = { time: this.nowtime() };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: dayjs().format("残り DD日 hh時間mm分ss秒"),
+    });
   }
 
   // date: string = moment().format("YYYY-MM-DD");
   render() {
     return (
       <>
-        <p>残り20日23時間23分12秒</p>
+        <p>{this.state.time}</p>
       </>
     );
   }
