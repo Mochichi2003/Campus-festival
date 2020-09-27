e45974fb77beb7631f179cf7a4c58dc984fba341
Mochichi2003 <44772513+Mochichi2003@users.noreply.github.com>
2020-09-27 16:05:12 +0900

    アニメーションを作った

--- pages/live.tsx
+++ pages/live.tsx
@@ -1,8 +1,8 @@
 // import Link from "next/link";
-import Layout from "../components/Layout";
+// import Layout from "../components/Layout";
 
 import React from "react";
-import styles from "../style/live.module.sass";
+// import styles from "../style/live.module.sass";
 import CountdownTimer from "../components/Countdowntimer/CountdownTimer";
 
 const Live = () => (
