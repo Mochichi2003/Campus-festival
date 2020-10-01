066b49d73a8078bf8dd5d880d04e1182128e5976
Mochichi2003 <moyurusuto@gmail.com>
2020-10-01 10:37:53 +0900

    aka

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,40 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-		node: true,
-	},
-	extends: [
-		"plugin:react/recommended",
-		"standard",
-		"plugin:prettier/recommended",
-	],
-	parser: "@typescript-eslint/parser",
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
-		ecmaVersion: 12,
-		sourceType: "module",
-	},
-	plugins: ["react", "@typescript-eslint"],
-	rules: {
-		"react/react-in-jsx-scope": "off",
-		"react/prop-types": "off",
-		"react/jsx-filename-extension": [
-			1,
-			{ extensions: [".js", ".jsx", ".tsx"] },
-		], // should add ".ts" if typescript project
-		"no-use-before-define": ["off", { functions: true, classes: true }],
-		"prettier/prettier": [
-			"error",
-			{
-				jsxBracketSameLine: true,
-				singleQuote: false,
-				trailingComma: "es5",
-				useTabs: true,
-			},
-		],
-	},
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "standard",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "no-useless-escape": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "react/jsx-filename-extension": [
+      1,
+      { extensions: [".js", ".jsx", ".tsx"] },
+    ], // should add ".ts" if typescript project
+    "no-use-before-define": ["off", { functions: true, classes: true }],
+    "prettier/prettier": [
+      "error",
+      {
+        jsxBracketSameLine: true,
+        singleQuote: false,
+        trailingComma: "es5",
+        useTabs: false,
+      },
+    ],
+  },
 };
