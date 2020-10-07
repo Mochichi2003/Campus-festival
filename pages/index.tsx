01ab4a75349ed48ce440058914cc75c1d26e4960
Mochichi2003 <moyurusuto@icloud.com>
2020-10-07 13:41:56 +0900

    見た目を良くした

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,10 +8,14 @@ const IndexPage = () => (
     title="Home"
     // NotShowHederAndFooter="true"
   >
-    <h1 className="text-6xl font-sans font-bold">HEY here is Top page👋</h1>
-    <Link href="./postdatas">
-      <a className="text-blue-500">展示物一覧</a>
-    </Link>
+    <h1 className="text-3xl md:text-6xl font-sans text-center	 font-bold">
+      HEY here is Top page🔔
+    </h1>
+    <div className="my-4 text-lg text-center	">
+      <Link href="./postdatas">
+        <a className="text-blue-500 ">展示物一覧</a>
+      </Link>
+    </div>
   </Layout>
 );
 
