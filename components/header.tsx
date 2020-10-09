78eac9bb329f6df9098a4dfce7cd0ca40623b564
Mochichi2003 <moyurusuto@gmail.com>
2020-10-09 14:44:59 +0900

    いろいろ直した

--- components/header.tsx
+++ components/header.tsx
@@ -12,8 +12,7 @@ export default function header() {
         className={
           styles.header_background +
           " flex items-center fixed w-screen flex-row  p-2 bg-gray-900 bg-opacity-95"
-        }
-      >
+        }>
         <div className="text-white  rounded-lg  cursor-pointer duration-300 hover:text-white text-center text-sm md:text-xl px-2  py-0 m-1">
           <Link href="/">
             <p className="font-medium">横浜CPキャンフェス</p>
