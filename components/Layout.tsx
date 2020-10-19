95bef23c0159191c1eb45a42fdc46ee60f3ab052
Mochichi2003 <moyurusuto@gmail.com>
2020-10-19 13:56:44 +0900

    アイコンを変えた

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -69,7 +69,34 @@ const Layout = ({
               href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
               rel="stylesheet"
             />
-            <link rel="icon" href="/icon/icon.png" />
+            {/* <link rel="icon" href="/icon/icon.png" /> */}
+            {/* faviconにかんして書いているところ 始まり */}
+            <link
+              rel="apple-touch-icon"
+              sizes="180x180"
+              href="/yokohamaN/apple-touch-icon.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="32x32"
+              href="/yokohamaN/favicon-32x32.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="16x16"
+              href="/yokohamaN/favicon-16x16.png"
+            />
+            <link rel="manifest" href="/yokohamaN/site.webmanifest" />
+            <link
+              rel="mask-icon"
+              href="/yokohamaN/safari-pinned-tab.svg"
+              color="#5bbad5"
+            />
+            <meta name="msapplication-TileColor" content="#1e5fa6" />
+            <meta name="theme-color" content="#ffffff"></meta>
+            {/* faviconにかんして書いているところ 終わり */}
             {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
           </Head>
         );
