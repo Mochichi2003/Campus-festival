fc1d81d4d462b7c1c90c456f3acf4540d0620623
Mochichi2003 <moyurusuto@gmail.com>
2020-10-22 12:26:13 +0900

    faviconを変更しました

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -74,28 +74,33 @@ const Layout = ({
             <link
               rel="apple-touch-icon"
               sizes="180x180"
-              href="/yokohamaN/apple-touch-icon.png"
+              href="/favicons/apple-touch-icon.png"
             />
             <link
               rel="icon"
               type="image/png"
               sizes="32x32"
-              href="/yokohamaN/favicon-32x32.png"
+              href="/favicons/favicon-32x32.png"
             />
             <link
               rel="icon"
               type="image/png"
               sizes="16x16"
-              href="/yokohamaN/favicon-16x16.png"
+              href="/favicons/favicon-16x16.png"
             />
-            <link rel="manifest" href="/yokohamaN/site.webmanifest" />
+            <link rel="manifest" href="/favicons/site.webmanifest" />
             <link
               rel="mask-icon"
-              href="/yokohamaN/safari-pinned-tab.svg"
+              href="/favicons/safari-pinned-tab.svg"
               color="#5bbad5"
             />
-            <meta name="msapplication-TileColor" content="#1e5fa6" />
-            <meta name="theme-color" content="#ffffff"></meta>
+            <link rel="shortcut icon" href="/favicons/favicon.ico" />
+            <meta name="msapplication-TileColor" content="#da532c" />
+            <meta
+              name="msapplication-config"
+              content="/favicons/browserconfig.xml"
+            />
+            <meta name="theme-color" content="#ffffff" />
             {/* faviconにかんして書いているところ 終わり */}
             {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
           </Head>
