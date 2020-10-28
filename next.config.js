b36cc8e476e640335e55d993f7c33ee1e6eb9d3b
Mochichi2003 <moyurusuto@icloud.com>
2020-10-28 12:18:09 +0900

    mdxで表示させたいんだけどどうやるんだ

--- next.config.js
+++ next.config.js
@@ -8,7 +8,14 @@
 // module.exports = withMDX({
 //   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 // });
-module.exports = {
+// next.config.js
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+});
+// module.exports = withMDX({
+//   pageExtensions: ['js', 'jsx', 'mdx'],
+// })
+module.exports = withMDX({
   target: "serverless",
   webpack: function (config) {
     config.module.rules.push({
@@ -17,4 +24,4 @@ module.exports = {
     });
     return config;
   },
-};
+});
