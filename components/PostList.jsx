d4405c4a55dd2cf074d91fe2b0c399d735019372
Mochichi2003 <moyurusuto@gmail.com>
2020-11-05 12:08:34 +0900

    良うわからん

--- components/PostList.jsx
+++ components/PostList.jsx
@@ -32,50 +32,58 @@ export default function PostList({ posts }) {
         {posts &&
           posts.map((post) => {
             return (
-              <div className={Style.PostList + "    "} key={post.slug}>
+              <>
                 <Link
                   href={{
                     pathname: `/post/${post.slug}`, // 投稿したやつのりんく
                   }}
                 >
-                  <div>
-                    <div className="postbtns  border-solid border rounded-lg border-gray-300  flex">
-                      <div className=" text-gray-700 text-center   ">
-                        {(() => {
-                          if (post.frontmatter.img) {
-                            // return <span>Good morning</span>;
-                            return (
-                              <div className="max-w-10 max-h-10 items-center   flex-row">
-                                <img
-                                  src={post.frontmatter.img}
-                                  alt="アイコン画像"
-                                  className={
-                                    Style.postlisticonimg +
-                                    "  border-gray-300 border m-2 rounded"
-                                  }
-                                  style={{}}
-                                />
-                              </div>
-                            );
-                          }
-                        })()}
-                      </div>
-                      <div className="p-3 sm:p-4 px-4 py-2">
-                        <a>
-                          <h2 className=" md:text-3xl text-left text-2xl font-bold">
-                            {post.frontmatter.title}
-                          </h2>
-                        </a>
-                        <p className=" text-left pt-1 hover_bulue">
-                          {post.frontmatter.author}
-                          {/* が書きました */}
-                        </p>
+                  <div
+                    className={
+                      Style.PostList +
+                      " border-solid border rounded-lg border-gray-300   "
+                    }
+                    key={post.slug}
+                  >
+                    <div>
+                      <div className="postbtns    flex">
+                        <div className=" text-gray-700 text-center   ">
+                          {(() => {
+                            if (post.frontmatter.img) {
+                              // return <span>Good morning</span>;
+                              return (
+                                <div className="max-w-10 max-h-10 items-center   flex-row">
+                                  <img
+                                    src={post.frontmatter.img}
+                                    alt="アイコン画像"
+                                    className={
+                                      Style.postlisticonimg +
+                                      "  border-gray-300 border m-2 rounded"
+                                    }
+                                    style={{}}
+                                  />
+                                </div>
+                              );
+                            }
+                          })()}
+                        </div>
+                        <div className="p-3 sm:p-4 px-4 py-2">
+                          <a>
+                            <h2 className=" md:text-3xl text-left text-2xl font-bold">
+                              {post.frontmatter.title}
+                            </h2>
+                          </a>
+                          <p className=" text-left pt-1 hover_bulue">
+                            {post.frontmatter.author}
+                            {/* が書きました */}
+                          </p>
+                        </div>
                       </div>
                     </div>
+                    {/* <hr /> */}
                   </div>
                 </Link>
-                {/* <hr /> */}
-              </div>
+              </>
             );
           })}
       </ul>
