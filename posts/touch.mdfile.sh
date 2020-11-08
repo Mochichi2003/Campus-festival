5b482b3bf6cf1c46b373ee1755aeca9de57b89a4
Mochichi2003 <moyurusuto@gmail.com>
2020-11-08 13:50:30 +0900

    生徒の投稿された記事を書いた

---
+++ posts/touch.mdfile.sh
@@ -0,0 +1,13 @@
+for i in {17..30}
+do
+  # touch "10${i}.md"
+  rm 10${i}.md 
+  echo "---
+title: \"\"
+author: \"\"
+# url: \"https://twitter.com/ajisaigohan\"
+# img: \"posts_icon/0${i}/icon.png\"
+---" >> 10${i}.md
+
+  echo "10${i}.md に書き込みました"
+done
--- public/posts_icon/mkdiricon.sh
+++ public/posts_icon/mkdiricon.sh
@@ -1,6 +1,7 @@
 for i in {17..30}
 do
-  rm -rf 
+  rm -rf "${i}"
+  echo "${i}を削除しました"
   echo "0${i}を作成しました"
   mkdir "0${i}"
 done
