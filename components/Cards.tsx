236faf823b9f6d3a700550a1a58898e484627a8a
Mochichi2003 <moyurusuto@gmail.com>
2020-11-13 14:29:13 +0900

    わーい

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -10,7 +10,7 @@ type CardProps = {
 };
 const Cards = ({ titel, body, img, height, width, children }: CardProps) => {
   return (
-    <div style={{}} className="w-full sm:w-2/4 md:w-1/3 h-full px-6 py-2 ">
+    <div style={{}} className="w-full sm:w-2/4 md:w-1/3 h-full px-6 py-6 ">
       {/* かーど transition duration-150 ease-in-out transform hover:scale-125 bg-blue-500 text-white font-bold py-2 px-4 rounded */}
       <div
         style={{
