d698d7db483d57c5718bffc3fa5e50c198d90c4e
Mochichi2003 <moyurusuto@gmail.com>
2020-11-18 13:32:37 +0900

    色々修正しました

--- components/posetest.jsx
+++ components/posetest.jsx
@@ -1,33 +1,33 @@
-import React, { useState } from "react";
-import posed from "react-pose";
+// import React, { useState } from "react";
+// import posed from "react-pose";
 
-const props = {
-  visible: {
-    opacity: 1,
-  },
-  hidden: {
-    opacity: 0.1,
-  },
-};
+// const props = {
+//   visible: {
+//     opacity: 1,
+//   },
+//   hidden: {
+//     opacity: 0.1,
+//   },
+// };
 
-const Box = posed.div(props);
+// const Box = posed.div(props);
 
-export default function ReactPoseSample() {
-  const [visible, setVisible] = useState(true);
+// export default function ReactPoseSample() {
+//   const [visible, setVisible] = useState(true);
 
-  return (
-    <></>
-    // <Box
-    //   style={{
-    //     margin: "0 auto",
-    //     width: 100,
-    //     height: 100,
-    //     backgroundColor: "green",
-    //   }}
-    //   pose={visible ? "visible" : "hidden"}
-    //   onClick={() => {
-    //     setVisible(!visible);
-    //   }}
-    // />
-  );
-}
+//   return (
+//     <></>
+//     // <Box
+//     //   style={{
+//     //     margin: "0 auto",
+//     //     width: 100,
+//     //     height: 100,
+//     //     backgroundColor: "green",
+//     //   }}
+//     //   pose={visible ? "visible" : "hidden"}
+//     //   onClick={() => {
+//     //     setVisible(!visible);
+//     //   }}
+//     // />
+//   );
+// }
