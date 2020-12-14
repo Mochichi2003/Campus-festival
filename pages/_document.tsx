66441ac6b333b5df9f7f802b749a5570456ccc4a
Mochichi2003 <moyurusuto@gmail.com>
2020-12-14 13:42:12 +0900

    google アナリティクスを削除

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,31 +1,13 @@
 // import Document, { Head, Main, NextScript } from "next/document";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
-import { GA_TRACKING_ID } from "../lib/gtag";
+
 
 export default class extends Document {
   render() {
     return (
       <Html lang="ja_JP">
-        <Head>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
-        </Head>
+        <Head>{/* Global Site Tag (gtag.js) - Google Analytics */}</Head>
         <body>
           <Main />
           <NextScript />
